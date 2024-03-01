b95ef77d24ec59b0dd6e0ec520b627690f5f8e87
mon1tor <westxlebyshek@gmail.com>
2024-03-01 13:55:45 +0100

    Possible crash fix

--- src/main/java/org/dawnoftimebuilder/mixin/client/CreativeInventoryHandler.java
+++ src/main/java/org/dawnoftimebuilder/mixin/client/CreativeInventoryHandler.java
@@ -2,9 +2,12 @@ package org.dawnoftimebuilder.mixin.client;
 
 import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import org.dawnoftimebuilder.HandlerClient;
 
 @SuppressWarnings("unused")
+@OnlyIn(Dist.CLIENT)
 public class CreativeInventoryHandler {
     public static void onCreativeTabChange(CreativeModeInventoryScreen screen, CreativeModeTab tab) {
         HandlerClient.onCreativeTabChange(screen, tab);
--- src/main/java/org/dawnoftimebuilder/mixin/impl/client/CreativeInventoryMixin.java
+++ src/main/java/org/dawnoftimebuilder/mixin/impl/client/CreativeInventoryMixin.java
@@ -2,6 +2,8 @@ package org.dawnoftimebuilder.mixin.impl.client;
 
 import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import org.dawnoftimebuilder.mixin.client.CreativeInventoryHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,6 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @SuppressWarnings("unused")
 @Mixin(CreativeModeInventoryScreen.class)
+@OnlyIn(Dist.CLIENT)
 public abstract class CreativeInventoryMixin {
     @Inject(method = "selectTab(Lnet/minecraft/world/item/CreativeModeTab;)V", at = @At(value = "RETURN"))
     public void selectTab(CreativeModeTab pTab, CallbackInfo ci) {
