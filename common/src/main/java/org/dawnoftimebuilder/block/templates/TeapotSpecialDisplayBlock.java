e418e7e7d29f4c10841583c9652a5422469fc941
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-06-10 22:46:59 -0300

    Implement smoke particle effect for cast iron teapots above Irori Fireplace

---
+++ common/src/main/java/org/dawnoftimebuilder/block/templates/TeapotSpecialDisplayBlock.java
@@ -0,0 +1,40 @@
+package org.dawnoftimebuilder.block.templates;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.dawnoftimebuilder.block.general.FireplaceBlock;
+import org.dawnoftimebuilder.platform.Services;
+import org.dawnoftimebuilder.registry.DoTBBlocksRegistry;
+
+public class TeapotSpecialDisplayBlock extends SpecialDisplayBlock {
+
+    public TeapotSpecialDisplayBlock(Properties properties, VoxelShape[] shapes) {
+        super(properties, shapes);
+    }
+
+    @Override
+    public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource rand) {
+        if (!level.isClientSide) return;
+
+        BlockState state1 = level.getBlockState(pos.below());
+        if (state1.getBlock() != DoTBBlocksRegistry.INSTANCE.IRORI_FIREPLACE.get()) return;
+
+        if (state1.getValue(FireplaceBlock.LIT) && rand.nextInt(5) == 0) {
+            SimpleParticleType particle;
+            if (Services.PLATFORM.isModLoaded("farmersdelight"))
+                particle = (SimpleParticleType) BuiltInRegistries.PARTICLE_TYPE.get(ResourceLocation.tryParse("farmersdelight:steam"));
+            else
+                particle = ParticleTypes.CAMPFIRE_COSY_SMOKE;
+
+            if (particle == null) return;
+            level.addParticle(particle, pos.getX() + 0.5, pos.getY() + 0.3D, pos.getZ() + 0.5, 0.0D, 0.01D, 0.0D);
+        }
+    }
+}
